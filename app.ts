574faadb04f17da24a43f37d3fcaea8b10d946bc
Florian Friedl <you@example.com>
2018-11-15 10:41:04 +0100

    Solution2.0_03_Friedl

--- app.ts
+++ app.ts
@@ -6,6 +6,30 @@ let valueEmail = $("#email").val();
 let valueNewsletter = $("#newsletter").prop('checked')
 let valueMCS = $("#mediaChannelSelect").val();
 
+if(valueFirstName == ""){
+    $("#firstNameMandatory").show();
+}else{
+    $("#firstNameMandatory").hide();
+}
+
+if(valueLastName == ""){
+    $("#lastNameMandatory").show();
+}else{
+    $("#lastNameMandatory").hide();
+}
+
+if(valueNewsletter = false){
+    $("#email").hide();
+}else{
+    $("#email").show();
+    if(valueEmail == ""){
+        $("#emailMandatory").show();
+    }else{
+        $("emailMandatory").hide();
+    }
+}
+
+
 if((valueFirstName == "" || valueLastName == "") && (valueNewsletter = true && valueEmail == "")){
     $("#submitButton").hide();
 }else{
--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,30 @@ $(document).on("load change", function () {
     var valueEmail = $("#email").val();
     var valueNewsletter = $("#newsletter").prop('checked');
     var valueMCS = $("#mediaChannelSelect").val();
+    if (valueFirstName == "") {
+        $("#firstNameMandatory").show();
+    }
+    else {
+        $("#firstNameMandatory").hide();
+    }
+    if (valueLastName == "") {
+        $("#lastNameMandatory").show();
+    }
+    else {
+        $("#lastNameMandatory").hide();
+    }
+    if (valueNewsletter = false) {
+        $("#email").hide();
+    }
+    else {
+        $("#email").show();
+        if (valueEmail == "") {
+            $("#emailMandatory").show();
+        }
+        else {
+            $("emailMandatory").hide();
+        }
+    }
     if ((valueFirstName == "" || valueLastName == "") && (valueNewsletter = true && valueEmail == "")) {
         $("#submitButton").hide();
     }
